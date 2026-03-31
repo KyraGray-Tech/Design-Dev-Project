6b7322cdfa858b7215449856d812a6604f7e7722
KyraGray-Tech <kyragraytech@gmail.com>
2026-03-30 22:19:42 -0500

    Add files via upload

--- Project/js/main.js
+++ Project/js/main.js
@@ -18,6 +18,30 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-submit.addEventListener("click", function() {
-  alert("Thank you for your submission. We'll review it and follow up soon!");
-})
+
+/* Tabs */
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  // Add border to current tab
+  this.classList.add('tab-border');
+  // Grab content item from the DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class
+  tabContentItem.classList.add('show');
+}
+
+function removeBorder() {
+  tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+  tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
